2de40aca8e33be3c07bc024893692b1f541fc80d
nax3t <iantskon@gmail.com>
2014-12-10 17:10:20 -0800

    Update app.js

--- app.js
+++ app.js
@@ -23,43 +23,20 @@ app.use(session({
   saveUninitialized: true
 }));
 
-// Hard-coded users
-var users = [ 
-    { id: 1, username: 'cho', password: 'pineapple', email: 'cho@ga.com' }
-];
-
-function findByUsername(username, fn) {
-  for (var i = 0, len = users.length; i < len; i++) {
-    var user = users[i];
-    if (user.username === username) {
-      return fn(null, user);
-    }
-  }
-  return fn(null, null);
-}
-
-function findById(id, fn) {
-  var idx = id - 1;
-  if (users[idx]) {
-    fn(null, users[idx]);
-  } else {
-    fn(new Error('User ' + id + ' does not exist'));
-  }
-}
-
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
 passport.deserializeUser(function(id, done) {
-  findById(id, function (err, user) {
+  db.query("SELECT * FROM users WHERE id = $1", [id], function (err, user) {
     done(err, user);
   });
 });
 
 var localStrategy = new LocalStrategy(
   function(username, password, done) {
-      findByUsername(username, function(err, user) {
+  	db.query("SELECT * FROM users WHERE username = $1", [username], function(err, dbRes) {
+        var user = dbRes.rows[0];
         if (err) { return done(err); }
         if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
         if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
@@ -98,8 +75,12 @@ app.post('/login', passport.authenticate('local',
 });
 
 app.get('/profile', function(req, res) {
-	console.log(req.user)
 	var user = req.user
+	// Debugging
+	console.log('-----------------------------------------');
+	console.log(user);
+	console.log('-----------------------------------------');
+	// Debug
 	res.render('profile', {user: user});
 });
 
